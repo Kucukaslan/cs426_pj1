2e7bbe06f9ec3f61a5af36125b767f06956d1d6e
Muhammed Can Küçükaslan <32893303+Kucukaslan@users.noreply.github.com>
2023-04-24 03:18:04 +0300

    implement mpi all reduce thing

--- average-mpi-ppv2.c
+++ average-mpi-ppv2.c
@@ -49,30 +49,32 @@ int main(int argc, char *argv[])
             // printf("Sent %d to %d of %d\n", numbers[0], i, size);
         }
 
-        int remaining = tot_count - count * (size - 1);
-        int *numbers = (int *)malloc(count * sizeof(int));
-        for (int i = 0; i < remaining; i++)
-        {
-            fscanf(fd, "%d", &numbers[i]);
-        }
-        fclose(fd);
-        printf("%d: call reduce\n", rank);
-        MPI_Allreduce(numbers, &sum, remaining, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-        int local_sum = 0;
-        for (int i = 0; i < remaining; i++)
-        {
-            local_sum += numbers[i];
-            // printf("%d: received %d, local sum %d\n", rank, numbers[i], local_sum);
-        }
-        printf("%d: all reduce sum %d; loc sum: %d\n", rank, sum, local_sum);
-        // // compute the sum of remaining numbers
-        // int tmp = 0;
-        // for (int i = count * (size - 1); i < tot_count; i++)
+        // int remaining = tot_count - count * (size - 1);
+        // int *numbers = (int *)malloc(count * sizeof(int));
+        // for (int i = 0; i < remaining; i++)
         // {
-        //     fscanf(fd, "%d", &tmp);
-        //     sum += tmp;
+        //     fscanf(fd, "%d", &numbers[i]);
         // }
         // fclose(fd);
+        // printf("%d: call reduce\n", rank);
+        // MPI_Allreduce(numbers, &sum, remaining, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+        // int local_sum = 0;
+        // for (int i = 0; i < remaining; i++)
+        // {
+        //     local_sum += numbers[i];
+        //     // printf("%d: received %d, local sum %d\n", rank, numbers[i], local_sum);
+        // }
+        // printf("%d: all reduce sum %d; loc sum: %d\n", rank, sum, local_sum);
+        // compute the sum of remaining numbers
+        int tmp = 0;
+        for (int i = count * (size - 1); i < tot_count; i++)
+        {
+            fscanf(fd, "%d", &tmp);
+            sum += tmp;
+        }
+        tmp = sum;
+        fclose(fd);
+        MPI_Allreduce(&tmp, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
         // // printf("Receiving local sums . . .%d\n", sum);
         // int local_sum = 0;
@@ -97,18 +99,18 @@ int main(int argc, char *argv[])
         MPI_Recv((void *)&count, 1, MPI_INT, 0, 0xACE5, MPI_COMM_WORLD, &s);
         // printf("%d Receiving %d numbers from master\n", rank, count);
         int *numbers = (int *)malloc(count * sizeof(int));
-        int *out = (int *)malloc(count * sizeof(int));
+        // int *out = (int *)malloc(count * sizeof(int));
         MPI_Recv(numbers, count, MPI_INT, 0, 0xACE5, MPI_COMM_WORLD, &s);
-        printf("%d Received %d numbers from master\n", rank, count);
+        // printf("%d Received %d numbers from master\n", rank, count);
         // printf("%d first number %d\n", rank, numbers[0]);
 
-        MPI_Allreduce(numbers, out, count, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
         for (int i = 0; i < count; i++)
         {
             local_sum += numbers[i];
-            printf("%d: received %d, local sum %d; out: %d\n", rank, numbers[i], local_sum, out[i]);
+            // printf("%d: received %d, local sum %d; \n", rank, numbers[i], local_sum);
         }
-        printf("%d: all reduce sum %d; loc sum: %d\n", rank, sum, local_sum);
+        MPI_Allreduce(&local_sum, &sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+        // printf("%d: all redmuce sum %d; loc sum: %d\n", rank, sum, local_sum);
 
         // // send the local sum to the master process
         // // printf("%d Sending local sum %d to master\n", rank, local_sum);
